863378747b21e223683679786bbdaef46215a5f9
Pattapol154 <sunfly-210@hotmail.com>
2020-08-09 02:43:31 +0700

    Updates

---
+++ precache-manifest.416ba9bd8d5e7e8048c33269c626befe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e2fe4bca1f4bc1fb52553c2ab2ade6",
+    "url": "/react-api-breakingbad/index.html"
+  },
+  {
+    "revision": "0c3c9b97f6eca3bef3f0",
+    "url": "/react-api-breakingbad/static/css/main.aa2b4a42.chunk.css"
+  },
+  {
+    "revision": "c4a3c650ba31b1217dcf",
+    "url": "/react-api-breakingbad/static/js/2.a9ad081e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api-breakingbad/static/js/2.a9ad081e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3c9b97f6eca3bef3f0",
+    "url": "/react-api-breakingbad/static/js/main.9275173a.chunk.js"
+  },
+  {
+    "revision": "d00a3d991c5034e079b6",
+    "url": "/react-api-breakingbad/static/js/runtime-main.91e78131.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-api-breakingbad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-api-breakingbad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-api-breakingbad/static/media/spinner.235b9b3e.gif"
+  }
+]);
